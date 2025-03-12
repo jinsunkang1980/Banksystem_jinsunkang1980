bd89ab8419f83e0cb79b5a4d8547bfa7428d408a
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-03-11 20:08:27 -0400

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public abstract class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    protected double balance;
 
-public class BankAccount {
+    public BankAccount(String accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = 0.0;
+    }
+
+    // Abstract methods
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount) throws InsufficientBalanceException;
+
+    // Getters and Setters
+    public String getAccountNumber() { return accountNumber; }
+    public String getAccountHolderName() { return accountHolderName; }
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
 }
