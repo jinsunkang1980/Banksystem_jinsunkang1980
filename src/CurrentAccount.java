8f226f3f109d5a38a3f8ba2e4d1f0f711e7ac708
jinsunkang1980 <jinsunkang1980@gmail.com>
2025-03-11 20:09:11 -0400

    Update CurrentAccount.java

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+public class CurrentAccount extends BankAccount implements AccountOperations {
+    private static final double OVERDRAFT_LIMIT = -1000.0;
 
-public class CurrentAccount {
+    public CurrentAccount(String accountNumber, String accountHolderName) {
+        super(accountNumber, accountHolderName);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful! New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    @Override
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount > 0) {
+            if (balance - amount < OVERDRAFT_LIMIT) {
+                throw new InsufficientBalanceException("Withdrawal exceeds overdraft limit of " + OVERDRAFT_LIMIT);
+            }
+            balance -= amount;
+            System.out.println("Withdrawal successful! New balance: " + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount!");
+        }
+    }
+
+    @Override
+    public void displayAccountDetails() {
+        System.out.println("Account Type: Current");
+        System.out.println("Account Number: " + getAccountNumber());
+        System.out.println("Holder Name: " + getAccountHolderName());
+        System.out.println("Balance: " + balance);
+    }
 }
